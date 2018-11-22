dfeb83334d90063403df2a2bdfbe85dbc7a5154b
375328825 <375328825@qq.com>
2018-11-22 22:00:59 +0800

    new model

--- UnityProject/MazeGame/Assets/Test.cs
+++ UnityProject/MazeGame/Assets/Test.cs
@@ -3,34 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Test : MonoBehaviour ,IPointerClickHandler{
+public class Test : MonoBehaviour {
 
 
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        Debug.Log("test");
-       // throw new NotImplementedException();
-    }
 
     // Use this for initialization
     void Start () {
-		
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
        
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if(Physics.Raycast(ray,out hit))
-            {
-               Debug.Log(hit.collider.gameObject.name);
-            }
-        }
+  
 	}
 
 }

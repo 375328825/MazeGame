7371bbec5096f266e1039b7fd64bbe776a91be89
Niko_ke <375328825@qq.com>
2018-11-30 17:50:23 +0800

    Add REsource

---
+++ UnityProject/MazeGame/Assets/CoinPusher/Scripts/CoinCoilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCoilder : MonoBehaviour {
+    int score = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        score++;
+    }
+    public void Reset()
+    {
+        score = 0;
+    }
+}
---
+++ UnityProject/MazeGame/Assets/CoinPusher/Scripts/CoinFAther.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinFAther : MonoBehaviour
+{
+    bool left = false;
+    public GameObject coinPrefab;
+    public GameObject coin;
+    // Use this for initialization
+    void Start()
+    {
+        coin = GameObject.Instantiate(coinPrefab, transform.position, coinPrefab.transform.rotation, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+        if (transform.localPosition.x >= 1.5f)
+        {
+
+            left = true;
+        }
+        else if (transform.localPosition.x <= -1.5f)
+        {
+            left = false;
+        }
+        if (left)
+        {
+            transform.localPosition += Vector3.left * Time.deltaTime;
+        }
+        else
+        {
+            transform.localPosition -= Vector3.left * Time.deltaTime;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (coin == null)
+            {
+                return;
+            }
+
+            coin.GetComponent<Rigidbody>().isKinematic = false;
+
+            coin.GetComponent<Rigidbody>().velocity = transform.forward * 9;
+            coin.transform.parent = null;
+            coin = null;
+            Invoke("InstantCoin", 0.3f);
+        }
+    }
+
+    public void InstantCoin()
+    {
+        coin = GameObject.Instantiate(coinPrefab, transform.position, coinPrefab.transform.rotation, transform);
+
+       // yield return 0;
+    }
+}
